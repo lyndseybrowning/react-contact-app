4ccdf5f808e4943a146ccf619c6f0d25123f7e49
Lyndsey Browning <lbrowning86@gmail.com>
2016-04-04 19:44:44 +0100

    Added SearchBar state and filter capability

--- src/js/components/ContactsApp.js
+++ src/js/components/ContactsApp.js
@@ -11,11 +11,17 @@ class ContactsApp extends Component {
     };
   }
 
+  handleUserInput(searchTerm) {
+    this.setState({ filterText: searchTerm })
+  }
+
   render() {
     return (
       <div>
-        <SearchBar filterText={this.state.filterText} />
-        <ContactList contacts={this.props.contacts} />
+        <SearchBar filterText={this.state.filterText}
+                   onUserInput={this.handleUserInput.bind(this)} />
+        <ContactList contacts={this.props.contacts}
+                     filterText={this.state.filterText} />
       </div>
     );
   }
