9c97888c8a73b92df59e8383aced1473b27d21ab
Lyndsey Browning <lbrowning86@gmail.com>
2016-04-04 20:03:33 +0100

    Moved contacts into JSON file, added container component that fetches data from JSON file

---
+++ src/js/components/ContactsAppContainer.js
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+import ContactsApp from './ContactsApp';
+import 'whatwg-fetch';
+
+class ContactsAppContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('./public/contacts.json')
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({ contacts: responseData });
+    })
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error);
+    });
+  }
+
+  render() {
+    return (
+      <ContactsApp contacts={this.state.contacts} />
+    );
+  }
+}
+
+export default ContactsAppContainer;
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import ContactsApp from './components/ContactsApp';
+import ContactsAppContainer from './components/ContactsAppContainer';
 
-let contacts = [
-  { name: "Cassio Zen", email: "cassiozen@gmail.com" },
-  { name: "Lyndsey Browning", email: "lbrowning86@gmail.com" },
-  { name: "Dan Abramov", email: "gaearon@somewhere.com" },
-  { name: "Pete Hunt", email: "floydophone@somewhere.com" },
-];
-
-render(<ContactsApp contacts={contacts} />, document.getElementById('app'));
+render(<ContactsAppContainer />, document.getElementById('app'));
