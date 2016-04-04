972eaba94213af407a65a701dabe3e36506a540e
Lyndsey Browning <lbrowning86@gmail.com>
2016-04-04 19:17:54 +0100

    Added search data

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,11 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import ContactsApp from './components/ContactsApp';
 
-render(<ContactsApp />, document.getElementById('app'));
+let contacts = [
+  { name: "Cassio Zen", email: "cassiozen@gmail.com" },
+  { name: "Lyndsey Browning", email: "lbrowning86@gmail.com" },
+  { name: "Dan Abramov", email: "gaearon@somewhere.com" },
+  { name: "Pete Hunt", email: "floydophone@somewhere.com" },
+];
+
+render(<ContactsApp contacts={contacts} />, document.getElementById('app'));
